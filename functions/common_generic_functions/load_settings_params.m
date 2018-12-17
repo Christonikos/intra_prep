b6872195618f4ad30e8cd838898c2b1711993cdb
Christos-Nikolaos ZACHAROPOULOS <czacharo@is150940.intra.cea.fr>
2018-12-17 16:45:12 +0100

    Modifications in : 1.load_settings_params, 2.load_raw_data, 3.runPreprocessing

--- functions/common_generic_functions/load_settings_params.m
+++ functions/common_generic_functions/load_settings_params.m
@@ -1,18 +1,39 @@
-function [settings, params] = load_settings_params(hard_drive_path, patid, recMethod, hopid)
+function [settings, params, P] = load_settings_params(hard_drive_path, hopid, P)
 % load_settings_params : Add the paths to the Data and the various
 % toolboxes needed for the analysis.
 %       INPUTS  :
 %                   1. hard_drive_path : String - The path to the data (provided by getCore.m)
 %                   2. patid           : String - The ID of the given
 %                                        patient (provided @ runPreprocessing.m)
-%                   3. recMethod       : String - The recording method ID (provided @ runPreprocessing.m). 
-%                   4. hopid           : String - The hospital-ID (provided @ runPreprocessing.m).
+%                   3. hopid           : String - The hospital-ID (provided @ runPreprocessing.m).
+%                   4. P               : Struct - The main configuration
+%                                                 struct (created @ runPreprocessing.m)
 %       OUTPUTS : 
 %                   1. settings        : Struct - Holds the paths to
 %                                                 various locations such as the data directory etc.
 %                   2. params          : Struct - Holds various hospital-specific information such as the 
 %                                                 soa of the paradigm and the notch filtering parameters.  
-%   
+%                   3. P               : Struct - Returns the configuration
+%                                                 struct including the following fields : 
+%                                                 3.1 : recording method (sEEG, Grid)
+%                                                 3.2 : median threshold -
+%                                                 This threshold will be
+%                                                 later used on the
+%                                                 rejection of channels
+%                                                 based on non-pathological
+%                                                 reasons. 
+%                                                 3.3 : spiking threshold -
+%                                                 This threshold will be
+%                                                 used to to detect spikes
+%                                                 (abrupt voltage changes)
+%                                                 in the signal.
+%                                                 3.4 : processing -
+%                                                 "quick"/"slow" : The option "slow" 
+%                                                 allows for visualization of the 
+%                                                 rejected electrodes at each stage 
+%                                                 of the pipeline.
+%                                                 
+%  
 % --------------------------------------------------------------------------------------------------------
 % Add the path to fieldtrip (if you need to open raw files using fieldtrip)
 % addpath(fullfile(core,'NeuroSyntax2', 'Code','External','MATLAB','ToolBoxes','fieldtrip_20171218'));
@@ -22,23 +43,24 @@ function [settings, params] = load_settings_params(hard_drive_path, patid, recMe
 % ft_defaults;
 % --------------------------------------------------------------------------------------------------------
 
-%------------------- PATIENT - CHOSEN IN THE RUN FUNCTION -------------------%
-settings.patient = patid;
 %------------------- HOSPITAL - CHOSEN IN THE RUN FUNCTION -------------------%
 switch hopid
     case 'Houston'
+        % -------- Patients -------- %
+        checkField(P,'patients',{'TS096'});
+        % get the patient-ID
+        patid               = P.patients{1};
+        settings.patient    = patid;
         % ------------------------------------- SETTINGS ------------------------------------- %
         settings.path2rawdata   = fullfile(hard_drive_path,'NeuroSyntax2','Data','Houston', settings.patient);
-        %------------------- Epoch paths -------------------%
-        settings.path2epochs    = fullfile(hard_drive_path,'NeuroSyntax2','Output', settings.patient,'epochs', filesep);
         %------------------- Figure paths -------------------%
         settings.path2figures   = fullfile(hard_drive_path,'NeuroSyntax2','Figures', settings.patient, filesep);
         %------------------- Output paths -------------------%
-        settings.path2output    = fullfile(hard_drive_path,'NeuroSyntax2','Output', settings.patient ,filesep); 
+        settings.path2output    = fullfile(hard_drive_path,'NeuroSyntax2','Output', settings.patient ,filesep);
         % ------------------------------------- PARAMETERS  ------------------------------------- %
         params.srate    = 2000;
         params.soa      = 0.532;
-        % Set the notch-filtering bandwidth 
+        % Set the notch-filtering bandwidth
         params.first_harmonic{1}    = 59;
         params.first_harmonic{2}    = params.first_harmonic{1} +2;
         params.second_harmonic{1}   = 119;
@@ -46,6 +68,31 @@ switch hopid
         params.third_harmonic{1}    = 179;
         params.third_harmonic{2}    = params.first_harmonic{1} +2;
         
+        % -------- Recording methods -------- %
+        checkField(P,'recordingmethod',{'sEEG'});
+        checkField(P,'datatype',{'Blackrock'});
+        % -------- Cleaning Thresholds --------     %
+        checkField(P,'medianthreshold', 5);         % distance from the median
+        checkField(P,'spikingthreshold',80);        % spiking threshold
+        checkField(P,'rereference','bipolar');      % commonaverage, clinical
+        checkField(P,'processing','quick');         %'slow'/'quick'.
+        checkField(P,'rawfilename','TS096_NeuroSyntax2_sEEG_files_a/20170606-111436-001.ns3')
+        
+        % -------------------- APPENDIX -------------------- %
+        % --- Houston Patients --- %
+        %   'TA719'
+        %   'TA724'
+        %   'TA750'
+        %   'TS083'
+        %   'TS096'
+        %   'TS097'
+        %   'TS100'
+        %   'TS101'
+        %   'TS104'
+        %   'TS107'
+        %   'TS109'
+        % ----------------------- %
+
     case 'UMC'
         % ------------------------------------- SETTINGS ------------------------------------- %
         settings.path2rawdata   = fullfile(hard_drive_path,'NijmegenHippocampus','Data',settings.patient);
@@ -81,8 +128,15 @@ switch hopid
         params.third_harmonic{2}    = params.first_harmonic{1} +2;
         
     case 'Marseille'
+        % -------- Recording methods -------- %
+        checkField(P,'recordingmethod',{'sEEG'});
+        checkField(P,'datatype',{'Blackrock'});
+        % -------- Cleaning Thresholds --------     %
+        checkField(P,'medianthreshold', 5);         % distance from the median
+        checkField(P,'spikingthreshold',80);        % spiking threshold
+        checkField(P,'rereference','bipolar');      % commonaverage, clinical
         % ------------------------------------- SETTINGS ------------------------------------- %
-        settings.path2rawdata   = fullfile(hard_drive_path, 'Marseille_MEG_SEEG','Data','RAW_DATA', settings.patient, recMethod, filesep);
+        settings.path2rawdata   = fullfile(hard_drive_path, 'Marseille_MEG_SEEG','Data','RAW_DATA', settings.patient, P.recordingmethod{1}, filesep);
         %------------------- Output paths -------------------%
         settings.path2output    = fullfile(hard_drive_path,'Marseille_MEG_SEEG','Output', settings.patient,recMethod ,filesep);
         %------------------- Figure paths -------------------%
--- runPreprocessing.m
+++ runPreprocessing.m
@@ -3,7 +3,7 @@
 % data from various research centers such as the Houston medical center,
 % the Marseille research center etc.
 % Written by : Christos-Nikolaos Zacharopoulos @UNICOG 2018
-%              christonik@gmail.com
+%              [christonik[at]gmail.com]
 % based on a similar pipeline used at the Stanford University.
 % This is a plug-and-play function, to change any input variable, provide it
 % as a pair-input  in the command window.
@@ -11,19 +11,17 @@
 % runPreprocessing('patients',{'TS096'},'medianthreshold',4,'spikingthreshold',80);
 % --------------------------------------------------------------------------------------------------
 function runPreprocessing(varargin)
-%% -----------  BRANCH 1 - SET THE PATHS ----------- %
+%% -----------  BRANCH 1 - SET AND ADD PATHS ----------- %
 % In this branch we define the paths for the user.
 clc;  close all;
-% Add the dependencies on the path
-addpath(genpath(fullfile(pwd,'functions','supportive_general_functions')));
-addpath(genpath(fullfile(pwd,'functions','common_generic_functions')));
-addpath(genpath(fullfile(pwd,'functions','runPreprocessing')));
 % Keep the workspace clean
 clearvars -except varargin
 %Check whether the debug mode is on
 if feature('IsDebugMode')
     dbquit all
 end
+% Add the dependencies on the path
+addpath(genpath(fullfile(pwd,'functions')));
 % Get the data path based on the hostname of the computer in use. The
 % variables are unaffected from the OS.
 [~,hard_drive_path, elecs_path] = getCore;
@@ -31,74 +29,23 @@ end
 % In this branch we manunally add the list of patients that corresponds to each
 % individual project.
 P = parsePairs(varargin);
-checkField(P,'processing','quick'); %'slow'/'quick'.
-% The option "slow" allows for visualization of the rejected electrodes at
-% each stage of the pipeline.
 checkField(P,'Hospital',{'Houston'});
 % get the hospital-ID
 hopID = P.Hospital{1};
-%% -----------  BRANCH 3 - SPECIFY HOSPITAL SPECIFIC PARAMETERS ----------- %
-% Here, we update the configuration structure P with the list of patients
-% and the recording methods that correspond to that hospital.
-switch hopID
-    % Get the list of patients and update the P structure
-    case 'Houston'
-        % -------- Patients -------- %
-        checkField(P,'patients',{'TS096'});
-        % -------- Recording methods -------- %
-        checkField(P,'recordingmethod',{'sEEG'}); % to do: integrate the grid method
-        checkField(P,'datatype',{'Blackrock'});
-        % -------- Cleaning Thresholds -------- %
-        checkField(P,'medianthreshold', 5);     % distance from the median
-        checkField(P,'spikingthreshold',80);    % spiking threshold
-        checkField(P,'rereference','bipolar');  % commonaverage, clinical
-end
-% get the patient-ID
-patid       = P.patients{1};
-% get the method-ID (sEEG, grid).
-recID       = P.recordingmethod{1};
-datatypeID  = P.datatype{1};
-%% -----------  BRANCH 4 - LOAD THE RAW DATA ----------- %
 % load settings and parameters
-[settings, params] = load_settings_params(hard_drive_path,patid, recID, hopID);
-% load the raw data : matrix of dimensions : [channels x time].
-% It contains the broadband activity from 
-[raw_data, ~ ,labels, gyri, ~,  params, recordings] = ...
-    load_raw_data(settings, P, params, datatypeID, elecs_path, recID, hopID);
-%% -----------  BRANCH 5 - MAIN  CHANNEL REJECTION ANALYSIS----------- %
+[settings, params, P]   = load_settings_params(hard_drive_path, hopID,P);
+recID                   = P.recordingmethod{1};
+datatypeID              = P.datatype{1};
+%% -----------  BRANCH 3 - LOAD THE RAW DATA ----------- %
+% load the raw data : matrix of dimensions : [channels x time]. 
+[raw_data, ~] = load_raw_data(settings, P, datatypeID, recID, hopid);
+%% -----------  BRANCH 4 - MAIN  CHANNEL REJECTION ANALYSIS----------- %
 % Memory pre-allocation
-clean_data       = cell(size(raw_data,1),size(raw_data,2)); 
-% clean_data       : Cell - Each entry corresponds to a recording session 
-%                           and is a matrix of dimensions : [channels x time]. 
-%                           It will contain the cleaned channels. 
-%                           The rejected channels will be  filled with NaNs.
-indexofcleandata = cell(size(raw_data,1),1);
-% indexofcleandata : Cell - Each entry corresponds to a recording session and
-%                           is a logical matrix of dimensions : [channels x 1].
-%                           It will contain the index of the rejected channels 
-%                           (0 == rejected).
-m = @mainPreProcessing;
-% loop through recordings
-for recording = 1:numel(recordings)
+clean_data       = zeros(size(raw_data,1),size(raw_data,2)); 
 % function handle to the main function.
-[clean_data{recording}, indexofcleandata{recording }] = ...
+[clean_data, indexofcleandata ] = ...
     m(P, settings, raw_data{recording }, params, labels, gyri, hopID, recording, recordings);
-end
 clear raw_data
-% -------------------- APPENDIX -------------------- %
-% --- Houston Patients --- %
-%   'TA719'
-%   'TA724'
-%   'TA750'
-%   'TS083'
-%   'TS096'
-%   'TS097'
-%   'TS100'
-%   'TS101'
-%   'TS104'
-%   'TS107'
-%   'TS109'
-% ----------------------- %
 
 
 
